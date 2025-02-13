92cb57cd0a392130d012e21df4d6ab6b02f1f5ac
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-02-13 21:12:11 +0530

    product component done

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Product from './components/Product/Product'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Product />
     </div>
   )
 }
