d1e1864c001c721b6077a7a5c5904814b66b4187
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-02-14 23:10:54 +0530

    all done

--- Ecommerce/src/components/Hero/Hero.jsx
+++ Ecommerce/src/components/Hero/Hero.jsx
@@ -34,7 +34,6 @@ const Hero = ({ handleOrderPopup }) => {
     arrows: false,
     infinite: true,
     speed: 800,
-    slidesToShow: 1, // Added this
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 4000,
@@ -46,12 +45,12 @@ const Hero = ({ handleOrderPopup }) => {
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
       {/* background pattern */}
-      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-8"></div>
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]"></div>
       {/* hero section */}
       <div className="container pb-8 sm:pb-0">
         <Slider {...settings}>
-          {ImageList.map((data, idx) => (
-            <div key={idx}>
+          {ImageList.map((data) => (
+            <div>
               <div className="grid grid-cols-1 sm:grid-cols-2">
                 {/* text content section */}
                 <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
@@ -91,12 +90,11 @@ const Hero = ({ handleOrderPopup }) => {
                     data-aos-once="true"
                     className="relative z-10"
                   >
-                  <img
-                    src={data.img}
-                    alt=""
-                    className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-[1.2] object-contain mx-auto"
-                  />
-
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"
+                    />
                   </div>
                 </div>
               </div>
--- Ecommerce/src/components/Navbar/Navbar.jsx
+++ Ecommerce/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import Logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
-import DarkMode from './DarkMode'
+import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown } from "react-icons/fa";
+import DarkMode from "./DarkMode";
 
-const menuItems = [
-  { id: 1, name: "Home", link: "/#" },
-  { id: 2, name: "Top Rated", link: "/#" },
-  { id: 3, name: "Kids Wear", link: "/#" },
-  { id: 4, name: "Women Wear", link: "/#" },
-  { id: 5, name: "Men Wear", link: "/#" },
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Electronics",
+    link: "/#",
+  },
 ];
 
-const dropdownLinks = [
+const DropdownLinks = [
   {
     id: 1,
     name: "Trending Products",
@@ -30,90 +51,78 @@ const dropdownLinks = [
   },
 ];
 
-const Navbar = () => {
-  const handleOrderClick = () => {
-    alert("Ordering is not available yet");
-  };
-
+const Navbar = ({ handleOrderPopup }) => {
   return (
-    <nav className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
-      {/* Upper Navbar */}
+    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+      {/* upper Navbar */}
       <div className="bg-primary/40 py-2">
         <div className="container flex justify-between items-center">
-          {/* Logo */}
           <div>
-            <a
-              href="#"
-              className="font-bold text-2xl sm:text-3xl flex gap-1 items-center"
-            >
-              <img src={Logo} alt="Shopsy logo" className="w-10" />
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <img src={Logo} alt="Logo" className="w-10" />
               Shopsy
             </a>
           </div>
 
-          {/* Search Bar */}
+          {/* search bar */}
           <div className="flex justify-between items-center gap-4">
             <div className="relative group hidden sm:block">
               <input
                 type="text"
-                placeholder="Search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-primary dark:border-gray-500 dark:bg-gray-800"
+                placeholder="search"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800  "
               />
-              <IoMdSearch className="absolute top-1/2 -translate-y-1/2 right-3 text-gray-500 group-hover:text-primary" />
+              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
             </div>
-          </div>
 
-          {/* Order Button */}
-          <button
-            onClick={handleOrderClick}
-            className="bg-gradient-to-t from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group"
-          >
-            <span className="hidden group-hover:block transition-all duration-200">
-              Order
-            </span>
-            <FaCartShopping className="text-xl drop-shadow-sm cursor-pointer" />
-          </button>
+            {/* order button */}
+            <button
+              onClick={() => handleOrderPopup()}
+              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white  py-1 px-4 rounded-full flex items-center gap-3 group"
+            >
+              <span className="group-hover:block hidden transition-all duration-200">
+                Order
+              </span>
+              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+            </button>
 
-          {/* Dark Mode Switch */}
-          <div>
-            <DarkMode />
+            {/* Darkmode Switch */}
+            <div>
+              <DarkMode />
+            </div>
           </div>
         </div>
       </div>
-
-      {/* Lower Navbar */}
-      <div className="flex justify-center">
-        <ul className="hidden sm:flex items-center gap-4">
-          {menuItems.map((item) => (
-            <li key={item.id}>
+      {/* lower Navbar */}
+      <div data-aos="zoom-in" className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((data) => (
+            <li key={data.id}>
               <a
-                href={item.link}
-                className="px-4 hover:text-primary transition-colors duration-200"
+                href={data.link}
+                className="inline-block px-4 hover:text-primary duration-200"
               >
-                {item.name}
+                {data.name}
               </a>
             </li>
           ))}
-
-          {/* Dropdown Menu */}
-          <li className="relative group cursor-pointer">
-            <a href="#" className="flex items-center gap-1 py-2">
+          {/* Simple Dropdown and Links */}
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-[2px] py-2">
               Trending Products
-              <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
             </a>
-
-            <div
-              className="absolute z-[9999] hidden group-hover:block w-[150px] rounded-md 
-              bg-white p-2 text-black shadow-md dark:bg-gray-800 dark:text-white"
-            >
+            <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
               <ul>
-                {dropdownLinks.map((link) => (
-                  <li key={link.id}>
+                {DropdownLinks.map((data) => (
+                  <li key={data.id}>
                     <a
-                      href={link.link}
-                      className="block w-full rounded-md p-2 hover:bg-primary/20"
+                      href={data.link}
+                      className="inline-block w-full rounded-md p-2 hover:bg-primary/20 "
                     >
-                      {link.name}
+                      {data.name}
                     </a>
                   </li>
                 ))}
@@ -122,7 +131,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-    </nav>
+    </div>
   );
 };
 
